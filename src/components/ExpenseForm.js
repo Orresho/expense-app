a4c3a2ab1ee1ae34f4e4701bf9321f25fc391cd9
unknown <orhan.hajderovic@hotmail.com>
2018-02-14 21:01:40 +0100

    ADD and EDIT expenses implemented

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -6,13 +6,19 @@ import 'react-dates/lib/css/_datepicker.css';
 const now = moment();
 
 export default class ExpenseForm extends React.Component {
+    constructor(props) {
+        super(props);
 
-    state = {
-        description: '',
-        note: '',
-        amount: '',
-        createdAt: moment(),
-        calendarFocused: false
+        this.state = {
+            // Check whether expense has any values
+            description: props.expense ? props.expense.description : '',
+            note: props.expense ? props.expense.note : '',
+            amount: props.expense ? (props.expense.amount / 100).toString() : '',
+            createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+            calendarFocused: false,
+            error: '',
+            buttonText: ''
+        }
     }
 
     onDescriptionChange = (e) => {
@@ -27,23 +33,48 @@ export default class ExpenseForm extends React.Component {
 
     onAmountChange = (e) => {
         const amount = e.target.value;
-        if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+        if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
             this.setState(() => ({ amount }))
         }
     }
 
     onDateChange = (createdAt) => {
-        this.setState(() => ({ createdAt }))
+        // If there is a created at value, set state
+        if (createdAt) {
+            this.setState(() => ({ createdAt }))
+        }
     }
 
-    onFocusChange = ({focused}) => {
+    onFocusChange = ({ focused }) => {
         this.setState(() => ({ calendarFocused: focused }))
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        if (!this.state.description || !this.state.amount) {
+            this.setState(() => ({ error: 'Please fill in the forms' }));
+
+        } else {
+            this.setState(() => ({ error: '' }))
+
+            // Object argument from AddExpensePage populated here with the values
+            this.props.onSubmit({
+                description: this.state.description,
+                amount: parseFloat(this.state.amount, 10) * 100,
+                createdAt: this.state.createdAt.valueOf(),
+                note: this.state.note
+
+            })
+
+        }
     }
 
     render() {
         return (
             <div>
-                <form>
+                <p>{this.state.error}</p>
+                <form onSubmit={this.onSubmit}>
                     <input
                         type="text"
                         placeholder="Description"
@@ -74,7 +105,10 @@ export default class ExpenseForm extends React.Component {
                             onChange={this.onNoteChange}
                         ></textarea>
                     </div>
-                    <button>Add Expense</button>
+                    <button>
+                        {this.state.amount !== '' && (this.state.buttonText = 'Edit Expenses')}
+                        {this.state.amount === '' && (this.state.buttonText = 'Add Expenses')}
+                    </button>
                 </form>
             </div>
         )
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { removeExpense } from '../actions/expenses';
-import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const ExpenseListItem = ({ dispatch, description, amount, createdAt, id }) => (
+
+const ExpenseListItem = ({ description, amount, createdAt, id }) => (
     <div>
+        <Link to={`/edit/${id}`}><h3>Description: {description}</h3></Link>
         <p>Description: {description} </p>
         <p>Amount: {amount}</p>
         <p>Created at: {createdAt}</p>
-        <button onClick={() => {
-            dispatch(removeExpense({ id }));
-        }}>Remove</button>
     </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
